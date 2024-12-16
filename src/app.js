951f053d4efbe03acf0d042295f23beea7ab0f81
thaibaocoder1 <thaibaok4.1@gmail.com>
2024-12-16 13:13:02 +0700

    chore: build source code #2

--- src/app.js
+++ src/app.js
@@ -7,13 +7,12 @@ const MongoDB = require('./configs/mongodb')
 
 const app = express()
 
-// middlewares
-
 const corsOptions = {
   origin: 'http://localhost:5173',
   credentials: true,
 }
 
+// middlewares
 app.use(cors(corsOptions))
 app.use(logger('dev'))
 app.use(express.json({ limit: '10mb' }))
@@ -23,10 +22,6 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')))
 
 // routes
 const apiRouter = require('./routes/apiRouter')
-// index route
-// app.use("/", (req, res, next) => {
-//   res.render("index");
-// });
 ;(async () => {
   try {
     console.log('Connecting to database...')
