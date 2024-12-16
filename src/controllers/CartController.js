eff54bdf9139eb5d6385aee3a95cd72f99d04490
thaibaocoder1 <thaibaok4.1@gmail.com>
2024-12-16 13:47:03 +0700

    fix: fix response

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -1,150 +1,156 @@
-const CartService = require("@/services/CartService");
-const VoucherService = require("@/services/VoucherService");
+const CartService = require('@/services/CartService')
+const VoucherService = require('@/services/VoucherService')
 
-const MapResponseMessage = require("@/utils/response-message/vi-VN");
+const MapResponseMessage = require('@/utils/response-message/vi-VN')
 
 class CartController {
-	constructor() {
-	}
+  constructor() {}
 
+  async addCakeToCart(req, res, next) {
+    try {
+      const { userCart } = await CartService.addCakeToCart(req._id, req.body)
 
-	async addCakeToCart(req, res, next) {
-		try {
+      const { numOfBranches, numOfProducts } =
+        CartService.getCartMetadata(userCart)
 
-			const { userCart } = await CartService.addCakeToCart(req._id, req.body);
+      return res.status(200).json({
+        status: 'success',
+        message: MapResponseMessage.addNewItemToCart,
+        metadata: {
+          customerId: req._id,
+          numOfBranches,
+          numOfProducts,
+        },
+        results: userCart,
+      })
+    } catch (error) {
+      return next({
+        status: 500,
+        error,
+      })
+    }
+  }
 
-			const { numOfBranches, numOfProducts } = CartService.getCartMetadata(userCart);
-			
-			return res.status(200).json({
-				status: "success",
-				message: MapResponseMessage.addNewItemToCart,
-				metadata: {
-					customerId: req._id,
-					numOfBranches,
-					numOfProducts
-				},
-				results: userCart
-			});
+  async changeCakeQuantity(req, res, next) {
+    try {
+      const newCart = await CartService.setCakeQuantity(req._id, req.body)
 
-		} catch (error) {
-			return next({
-				status: 500,
-				error
-			});
-		}
-	}
+      if (!newCart) {
+        return next({
+          status: 400,
+          message: MapResponseMessage.notFound('Bánh trong giỏ hàng'),
+        })
+      }
 
-	async changeCakeQuantity(req, res, next) {
-		try {
+      const { userCart } = newCart
 
-			const newCart = await CartService.setCakeQuantity(req._id, req.body);
+      const { numOfBranches, numOfProducts } =
+        CartService.getCartMetadata(userCart)
 
-			if (!newCart) {
-				return next({
-					status: 400,
-					message: MapResponseMessage.notFound("Bánh trong giỏ hàng")
-				});
-			}
+      return res.status(200).json({
+        status: 'success',
+        message: MapResponseMessage.successUpdate('Số lượng của bánh'),
+        metadata: {
+          customerId: req._id,
+          numOfBranches,
+          numOfProducts,
+        },
+        results: userCart,
+      })
+    } catch (error) {
+      return next({
+        status: 500,
+        error,
+      })
+    }
+  }
 
-			const { userCart } = newCart;
+  async deleteCake(req, res, next) {
+    try {
+      let { userCart } = await CartService.deleteCakeFromCart(req._id, req.body)
 
-			const { numOfBranches, numOfProducts } = CartService.getCartMetadata(userCart);
+      const { numOfBranches, numOfProducts } =
+        CartService.getCartMetadata(userCart)
 
-			return res.status(200).json({
-				status: "success",
-				message: MapResponseMessage.successUpdate("Số lượng của bánh"),
-				metadata: {
-					customerId: req._id,
-					numOfBranches,
-					numOfProducts
-				},
-				results: userCart
-			});
+      return res.status(200).json({
+        status: 'success',
+        message: MapResponseMessage.successDeleted('Bánh khỏi giỏ hàng'),
+        metadata: {
+          customerId: req._id,
+          numOfBranches,
+          numOfProducts,
+        },
+        results: userCart,
+      })
+    } catch (error) {
+      return next({
+        status: 500,
+        error,
+      })
+    }
+  }
 
-		} catch (error) {
-			return next({
-				status: 500,
-				error
-			});
-		}
-	}
+  async resetCart(req, res, next) {
+    try {
+      const { userCart } = await CartService.resetCustomerCart(req._id)
 
-	async deleteCake(req, res, next) {
-		try {
-			let { userCart } = await CartService.deleteCakeFromCart(req._id, req.body);
+      const { numOfBranches, numOfProducts } =
+        CartService.getCartMetadata(userCart)
 
-			const { numOfBranches, numOfProducts } = CartService.getCartMetadata(userCart);
+      return res.status(200).json({
+        status: 'success',
+        message: MapResponseMessage.resetCart,
+        metadata: {
+          customerId: req._id,
+          numOfBranches,
+          numOfProducts,
+        },
+        results: userCart,
+      })
+    } catch (error) {
+      return next({
+        status: 500,
+        error,
+      })
+    }
+  }
 
-			return res.status(200).json({
-				status: "success",
-				message: MapResponseMessage.successDeleted("Bánh khỏi giỏ hàng"),
-				metadata: {
-					customerId: req._id,
-					numOfBranches,
-					numOfProducts
-				},
-				results: userCart
-			});
-		} catch (error) {
-			return next({
-				status: 500,
-				error
-			});
-		}
-	}
+  async setVoucherCode(req, res, next) {
+    try {
+      const { voucherCode, orderData } = req.body
 
-	async resetCart(req, res, next) {
-		try {
-			const { userCart } = await CartService.resetCustomerCart(req._id);
+      const checkUsability = await VoucherService.checkVoucherUsability(
+        req._id,
+        voucherCode,
+        orderData,
+        orderData.branchId
+      )
 
-			const { numOfBranches, numOfProducts } = CartService.getCartMetadata(userCart);
+      if (!checkUsability.isOk) {
+        return res.status(400).json({
+          status: 'failure',
+          message: checkUsability.message,
+        })
+      }
 
-			return res.status(200).json({
-				status: "success",
-				message: MapResponseMessage.resetCart,
-				metadata: {
-					customerId: req._id,
-					numOfBranches,
-					numOfProducts
-				},
-				results: userCart
-			});
-		} catch (error) {
-			return next({
-				status: 500,
-				error
-			});
-		}
-	}
+      await CartService.setVoucherForBranchInCart(
+        req._id,
+        voucherCode,
+        orderData.branchId
+      )
 
-	async setVoucherCode(req, res, next) {
-		try {
-			const { voucherCode, orderData } = req.body;
-
-			const checkUsability = await VoucherService.checkVoucherUsability(req._id, voucherCode, orderData, orderData.branchId);
-
-			if (!checkUsability.isOk) {
-				return next({
-					status: 400,
-					message: checkUsability.message
-				});
-			}
-
-			await CartService.setVoucherForBranchInCart(req._id, voucherCode, orderData.branchId);
-
-			return res.status(200).json({
-				status: "success",
-				message: MapResponseMessage.successUpdate("Mã giảm giá"),
-				results: checkUsability.voucherData
-			});
-
-		} catch (error) {
-			return next({
-				status: 500,
-				error
-			});
-		}
-	}
+      return res.status(200).json({
+        status: 'success',
+        message: MapResponseMessage.successUpdate('Mã giảm giá'),
+        results: checkUsability.voucherData,
+      })
+    } catch (error) {
+      return next({
+        status: 500,
+        error,
+      })
+    }
+  }
 }
 
-module.exports = new CartController();
+module.exports = new CartController()
