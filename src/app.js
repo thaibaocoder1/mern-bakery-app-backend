1c1c4091e0a104811a8073e316c2bc2e24747325
thaibaocoder1 <thaibaok4.1@gmail.com>
2024-12-16 13:16:41 +0700

    fix: fix params cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,10 @@ const MongoDB = require('./configs/mongodb')
 const app = express()
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: [
+    'https://mern-bakery-app-frontend.vercel.app',
+    'http://localhost:5173',
+  ],
   credentials: true,
 }
 
